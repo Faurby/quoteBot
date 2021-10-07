3000db342ed16f74c7223cb4f419da09d1e14a4f
Lasse Faurby Klausen <lasse_fk@hotmail.com>
2021-10-08 00:56:31 +0200

    initial bot up n running

--- main.go
+++ main.go
@@ -2,21 +2,65 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"math/rand"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	Token string
 )
 
 var quotes map[string]string
+var path string = "data\\quote.txt"
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot token")
+	flag.Parse()
+}
 
 func main() {
-	path := "data\\quote.txt"
 	quotes = ParseFile(path)
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		log.Fatalf("Error creating Discord session %v", err)
+	}
+	dg.AddHandler(messageCreate)
+
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = dg.Open()
+	if err != nil {
+		log.Fatalf("Error opening connection %v", err)
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
 
 	GuessRandomQuote()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "!quote" {
+		FindRandomQuote()
+	}
 }
 
 func ParseFile(path string) map[string]string {
